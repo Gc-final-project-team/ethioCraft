3a749e86eaf66864e6674e6e0169584a5c3ba249
yididiya <yididiya07@gmail.com>
2023-12-27 21:59:47 +0300

    adding routes to the pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './pages/login';
+import Signup from './pages/signup';
+import Landing from './pages/hero';
 
 function App() {
   return (
-    <div className="App">
-     <h1> React app</h1>
-    </div>
+    <Router>
+    <Routes>
+    <Route path='/' element={<Landing/>}></Route>
+    <Route path='/signup' element={<Signup/>}></Route>
+
+      </Routes>
+      </Router>
+    
   );
 }
 
