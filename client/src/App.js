51566b21af7ef71c7da4f6ed44fbaebbeadb982e
yididiya <yididiya07@gmail.com>
2024-01-17 19:07:30 +0300

    adding routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/login';
 import Signup from './pages/signup';
 import Landing from './pages/hero';
+import ThemeSwitcher from './pages/ThemeSwitcher';
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
     <Routes>
     <Route path='/' element={<Landing/>}></Route>
     <Route path='/signup' element={<Signup/>}></Route>
+    <Route path='/login' element={<Login/>}></Route>
+    <Route path='/dark' element={<ThemeSwitcher/>}></Route>
 
       </Routes>
       </Router>
